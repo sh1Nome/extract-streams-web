cbb6ae88cc37b7056c47f2191cb9f60fcb398dd9
sh1Nome <shinodayo1024@gmail.com>
2025-04-07 22:19:36 +0900

    update

--- python/apisource/main.py
+++ python/apisource/main.py
@@ -1,7 +1,14 @@
-from fastapi import FastAPI
+import io
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import StreamingResponse
+from urllib.parse import quote
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World!"}
+@app.post("/extract-audio")
+async def extract_audio(file: UploadFile = File(...)):
+    # ファイルをメモリ上で保持
+    file_content = await file.read()
+
+    # メモリ上のファイルをそのまま返す
+    return StreamingResponse(io.BytesIO(file_content), media_type="video/mp4")
