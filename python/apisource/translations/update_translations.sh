8e78c59fcb4fdffb891475c152e471b8be7922a4
sh1Nome <shinodayo1024@gmail.com>
2025-04-13 16:07:54 +0900

    翻訳スクリプトのコメントを日本語に翻訳し、メッセージを明確にしました。

--- python/apisource/translations/update_translations.sh
+++ python/apisource/translations/update_translations.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-# Navigate to the script's directory
+# スクリプトのディレクトリに移動
 cd "$(dirname "$0")"
 
-# Extract new messages and update messages.pot
+# 新しいメッセージを抽出してmessages.potを更新
 ~/.local/bin/pybabel extract -F babel.cfg -o messages.pot ..
 
-# Update .po files with new messages
+# 新しいメッセージで.poファイルを更新
 ~/.local/bin/pybabel update -i messages.pot -d .
 
-# Compile .po files into .mo files
+# .poファイルをコンパイルして.moファイルを生成
 ~/.local/bin/pybabel compile -d .
 
-# Remove the POT-Creation-Date line from messages.pot
+# POT-Creation-Dateは翻訳テンプレートが生成された日時を示すメタデータです。
+# この処理は、スクリプトを実行するたびにPOT-Creation-Dateの差分が出ないようにするためです。
+# 具体的には、sedコマンドを使用して以下のファイルからPOT-Creation-Date行を削除することで、日時の変更による不要な差分を防いでいます。
+# - messages.pot
+# - 各言語ディレクトリ内のmessages.po (例: en/LC_MESSAGES/messages.po, ja/LC_MESSAGES/messages.po)
 sed -i '/^"POT-Creation-Date:/d' messages.pot
-
-# Remove the POT-Creation-Date line from all .po files
 find . -name "messages.po" -exec sed -i '/^"POT-Creation-Date:/d' {} +
 
-echo "Translation files updated and compiled successfully."
+echo "翻訳ファイルが正常に更新およびコンパイルされました。"
