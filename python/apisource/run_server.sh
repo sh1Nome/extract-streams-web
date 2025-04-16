af6dcef82c3ac02dedd5320b2d296359a59f90b4
sh1Nome <shinodayo1024@gmail.com>
2025-04-17 00:02:09 +0900

    Docker Composeファイルを追加し、開発環境と本番環境の設定を整備しました。また、サーバー起動スクリプトを修正し、Uvicornサーバーを正しく起動するようにしました。

--- python/apisource/run_server.sh
+++ python/apisource/run_server.sh
@@ -4,7 +4,4 @@
 sh /home/work/apisource/translations/update_translations.sh
 
 # Uvicornサーバーを起動
-# ../.local/bin/uvicorn main:app --host 0.0.0.0
-
-# スクリプトが終了しないようにする (開発用の記述: Dockerコンテナが自動的に停止しないようにするため)
-sleep infinity
+../.local/bin/uvicorn main:app --host 0.0.0.0
