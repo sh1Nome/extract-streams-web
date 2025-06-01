27a137bc89656e687d8e9a69ace28c470d36669c
sh1nome <shinodayo1024@gmail.com>
2025-06-01 18:33:01 +0900

    feat: 音声トラック抽出機能を追加し、ファイル入力コンポーネントを実装

--- node/frontend/src/App.vue
+++ node/frontend/src/App.vue
@@ -1,6 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+
+const file = ref<File | null>(null);
+
+function handleExtractAudio() {
+  if (file.value) {
+    console.log(file.value.name);
+  } else {
+    console.log("ファイルが選択されていません");
+  }
+}
+</script>
 
 <template>
-  <v-icon icon="mdi-home" />
-  <v-card text="Hello World!" variant="tonal"></v-card>
+  <v-file-input label="File input" v-model="file"></v-file-input>
+  <v-btn @click="handleExtractAudio">音声トラックを抽出</v-btn>
 </template>
