1860e3b05caae9b74ff2e3949bd1bb7bcc9a29d5
sh1nome <shinodayo1024@gmail.com>
2025-09-21 22:15:00 +0900

    [update]デザインを変更
    単なるHTMLからVuetifyのデザインに変更しました。

--- node/frontend/src/App.vue
+++ node/frontend/src/App.vue
@@ -1,12 +1,24 @@
 <template>
   <!-- TODO: actionのハードコードをやめる -->
-  <form
-    :action="'http://localhost:8000/api/v1/extract_audio'"
-    method="POST"
-    enctype="multipart/form-data"
-    target="_blank"
-  >
-    <input type="file" name="file" accept="video/*" required />
-    <button type="submit">音声トラックを抽出</button>
-  </form>
+  <v-container class="d-flex justify-center align-center" style="height: 100vh">
+    <v-form
+      :action="'http://localhost:8000/api/v1/extract_audio'"
+      method="POST"
+      enctype="multipart/form-data"
+      target="_blank"
+      style="width: 400px"
+    >
+      <v-file-input
+        name="file"
+        accept="video/*"
+        label="動画ファイルを選択"
+        required
+        prepend-icon="mdi-video"
+        variant="outlined"
+      ></v-file-input>
+      <v-btn type="submit" color="primary" class="mt-4" block>
+        音声トラックを抽出
+      </v-btn>
+    </v-form>
+  </v-container>
 </template>
